7ea3dd9b74b7303a4bdfd9546e705eebda6922d6
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2021-10-18 12:40:20 +0300

    Update ft_three_str.cpp

--- ft_three_str.cpp
+++ ft_three_str.cpp
@@ -1,6 +1,6 @@
 #include "str_easy.h"
 
-string ft_three_str(string str1, string str2, string str3){
+string itc_three_str(string str1, string str2, string str3){
     string newstr = "";
     for(int i = 0; i < ft_len(str1); i++){
         if(ft_find_str(ft_slice_str(str1, i, i + ft_len(str2) - 1), str2) != -1){
