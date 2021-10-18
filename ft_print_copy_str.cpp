6ed30fa271c0229d5b5320af551c0e4412bd49b3
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2021-10-18 12:39:47 +0300

    Update ft_print_copy_str.cpp

--- ft_print_copy_str.cpp
+++ ft_print_copy_str.cpp
@@ -1,6 +1,6 @@
 #include "str_easy.h"
 
-void ft_print_copy_str(string str, int number){
+void itc_print_copy_str(string str, int number){
     while(number != 0){
         cout << str << endl;
         number--;
