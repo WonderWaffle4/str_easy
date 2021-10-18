1dc0080f7a3322b4943376a4a4df9b62b03971d9
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2021-10-18 12:38:49 +0300

    Update ft_find_str.cpp

--- ft_find_str.cpp
+++ ft_find_str.cpp
@@ -1,6 +1,6 @@
 #include "str_easy.h"
 
-int ft_find_str(string str1, string str2){
+int itc_find_str(string str1, string str2){
     for(int i = 0; ft_len(ft_slice_str(str1, i, i + ft_len(str2) - 1)) == ft_len(str2); i++){
         if(ft_slice_str(str1, i, i + ft_len(str2) - 1) == str2) return i;
     }
