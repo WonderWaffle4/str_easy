5379e7e3831e58468467a7248510fc1a62617a64
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2021-10-18 12:46:00 +0300

    Update ft_count_char_in_str.cpp

--- ft_count_char_in_str.cpp
+++ ft_count_char_in_str.cpp
@@ -2,7 +2,7 @@
 
 int itc_count_char_in_str(char sym, string str){
     int kol = 0;
-    for(int i = 0; i <= ft_len(str); i++){
+    for(int i = 0; i <= itc_len(str); i++){
         if(sym == str[i]) kol++;
     }
     return kol;
