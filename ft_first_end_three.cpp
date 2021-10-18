205621eebb1a776bde5073e89f947ba141eccaee
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2021-10-18 12:44:53 +0300

    Update ft_first_end_three.cpp

--- ft_first_end_three.cpp
+++ ft_first_end_three.cpp
@@ -1,16 +1,16 @@
 #include "str_easy.h"
 
 void itc_first_end_three(string str){
-    if(ft_len(str) > 5){
+    if(itc_len(str) > 5){
         cout << str[0] << endl;
         cout << str[1] << endl;
         cout << str[2] << endl;
-        cout << str[ft_len(str) - 3] << endl;
-        cout << str[ft_len(str) - 2] << endl;
-        cout << str[ft_len(str) - 1] << endl;
+        cout << str[itc_len(str) - 3] << endl;
+        cout << str[itc_len(str) - 2] << endl;
+        cout << str[itc_len(str) - 1] << endl;
     }
     else{
-        for(int i = ft_len(str); i != 0; i--){
+        for(int i = itc_len(str); i != 0; i--){
             cout << str[0];
         }
     }
