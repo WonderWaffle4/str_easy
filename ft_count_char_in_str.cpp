9132c08e3c6b4b78e56f91ebd06918afb68e6256
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2021-10-18 12:38:20 +0300

    Update ft_count_char_in_str.cpp

--- ft_count_char_in_str.cpp
+++ ft_count_char_in_str.cpp
@@ -1,6 +1,6 @@
 #include "str_easy.h"
 
-int ft_count_char_in_str(char sym, string str){
+int itc_count_char_in_str(char sym, string str){
     int kol = 0;
     for(int i = 0; i <= ft_len(str); i++){
         if(sym == str[i]) kol++;
