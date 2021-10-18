da5f8e3384dd8d4a5f052c57a06a3b9e82fdb4d1
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2021-10-18 12:36:58 +0300

    Update ft_cmp_str.cpp

--- ft_cmp_str.cpp
+++ ft_cmp_str.cpp
@@ -1,6 +1,6 @@
 #include "str_easy.h"
 
-string ft_cmp_str(string str1, string str2, int num){
+string itc_cmp_str(string str1, string str2, int num){
     string newstr = "";
     newstr += ft_slice_str(str1, 0, num);
     newstr += str2;
