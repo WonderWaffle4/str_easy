8ee20f4d1b7373bbf778c5b98438ab95eda61d9d
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2021-10-18 12:38:39 +0300

    Update ft_even_place.cpp

--- ft_even_place.cpp
+++ ft_even_place.cpp
@@ -1,6 +1,6 @@
 #include "str_easy.h"
 
-string ft_even_place(string str){
+string itc_even_place(string str){
     string newstr;
     for(int i = 1; i < ft_len(str); i+=2){
         newstr += str[i];
