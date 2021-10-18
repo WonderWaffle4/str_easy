aaf7dce24cbdb254e3f9ff159a67df46b02041ad
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2021-10-18 12:37:48 +0300

    Update str_easy.h

--- str_easy.h
+++ str_easy.h
@@ -6,23 +6,23 @@
 
 using namespace std;
 
-string ft_hello_str(string name);
-string ft_even_place(string str);
-string ft_reverse_str(string str);
-string ft_slice_str(string str, int start, int last);
-string ft_cmp_str(string str1, string str2, int num);
-string ft_three_str(string str1, string str2, string str3);
+string itc_hello_str(string name);
+string itc_even_place(string str);
+string itc_reverse_str(string str);
+string itc_slice_str(string str, int start, int last);
+string itc_cmp_str(string str1, string str2, int num);
+string itc_three_str(string str1, string str2, string str3);
 
-void ft_print_copy_str(string str, int number);
-void ft_first_end_three(string str);
+void itc_print_copy_str(string str, int number);
+void itc_first_end_three(string str);
 
-int ft_len(string str);
-int ft_count_char_in_str(char, string str);
-int ft_find_str(string str1, string str2);
-int ft_max_char_on_end(string str);
+int itc_len(string str);
+int itc_count_char_in_str(char, string str);
+int itc_find_str(string str1, string str2);
+int itc_max_char_on_end(string str);
 
 
-double ft_percent_lower_uppercase(string str);
+double itc_percent_lower_uppercase(string str);
 
-bool ft_equal_reverse(string str);
+bool itc_equal_reverse(string str);
 #endif // STR_EASY_H_INCLUDED
