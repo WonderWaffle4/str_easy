3b2cf73b9a80466cb23aaaf85e877a4d19d803d3
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2021-10-18 12:44:24 +0300

    Update ft_max_char_on_end.cpp

--- ft_max_char_on_end.cpp
+++ ft_max_char_on_end.cpp
@@ -3,7 +3,7 @@
 int itc_max_char_on_end(string str){
     int kol = 1;
     int maxkol = 0;
-    for(int i = 0; i < ft_len(str); i++){
+    for(int i = 0; i < itc_len(str); i++){
         if(str[i] >= 48 && str[i] <= 57 && str[i + 1] >= 48 && str[i + 1] <= 57){
             kol++;
         }
