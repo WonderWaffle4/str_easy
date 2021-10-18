926c8b911e73ba02bc3e145ef87ce2573edc35f5
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2021-10-18 12:44:14 +0300

    Update ft_percent_lower_uppercase.cpp

--- ft_percent_lower_uppercase.cpp
+++ ft_percent_lower_uppercase.cpp
@@ -3,7 +3,7 @@
 double itc_percent_lower_uppercase(string str){
     double stroch = 0;
     double prop = 0;
-    for(int i = 0; i < ft_len(str); i++){
+    for(int i = 0; i < itc_len(str); i++){
         if(str[i] >= 65 && str[i] <= 90) prop++;
         if(str[i] >= 97 && str[i] <= 122) stroch++;
     }
