8d82b43cb0dc818371d19c9607d427962fd8e896
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2021-10-18 12:43:56 +0300

    Update ft_reverse_str.cpp

--- ft_reverse_str.cpp
+++ ft_reverse_str.cpp
@@ -2,7 +2,7 @@
 
 string itc_reverse_str(string str){
     string newstr = "";
-    for(int i = ft_len(str) - 1; i >= 0; i--){
+    for(int i = itc_len(str) - 1; i >= 0; i--){
         newstr += str[i];
     }
     return newstr;
