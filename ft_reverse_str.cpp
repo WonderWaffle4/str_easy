0f0f668126a2c27164e3536a861eae2b4a8a836d
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2021-10-18 12:40:03 +0300

    Update ft_reverse_str.cpp

--- ft_reverse_str.cpp
+++ ft_reverse_str.cpp
@@ -1,6 +1,6 @@
 #include "str_easy.h"
 
-string ft_reverse_str(string str){
+string itc_reverse_str(string str){
     string newstr = "";
     for(int i = ft_len(str) - 1; i >= 0; i--){
         newstr += str[i];
