a68389186b40af7851816c5d63f23416a520c7b7
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2021-10-18 12:48:09 +0300

    Update ft_three_str.cpp

--- ft_three_str.cpp
+++ ft_three_str.cpp
@@ -2,7 +2,7 @@
 
 string itc_three_str(string str1, string str2, string str3){
     string newstr = "";
-    for(int i = 0; i < ft_len(str1); i++){
+    for(int i = 0; i < itc_len(str1); i++){
         if(itc_find_str(itc_slice_str(str1, i, i + itc_len(str2) - 1), str2) != -1){
             newstr+= str3;
         }
