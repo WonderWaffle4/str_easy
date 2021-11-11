e97ef3b8f2ccecdf8ab49966efdc807dfefbf6cb
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2021-11-11 11:20:54 +0300

    Update ft_slice_str.cpp

--- ft_slice_str.cpp
+++ ft_slice_str.cpp
@@ -2,7 +2,7 @@
 
 string itc_slice_str(string str, int start, int last){
     string newstr = "";
-    if(last > itc_len(str)) last == itc_len(str) - 1;
+    if(last > itc_len(str)) last = itc_len(str) - 1;
     for(int i = start; i <= last; i++) newstr += str[i];
     return newstr;
 }
