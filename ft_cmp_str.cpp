00cdc64da4485a21b12c88d5319371ef4d9eef1c
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2021-11-11 11:30:17 +0300

    Update ft_cmp_str.cpp

--- ft_cmp_str.cpp
+++ ft_cmp_str.cpp
@@ -2,8 +2,8 @@
 
 string itc_cmp_str(string str1, string str2, int num){
     string newstr = "";
-    newstr += itc_slice_str(str1, 0, num);
+    newstr += itc_slice_str(str1, 1, num);
     newstr += str2;
-    newstr += itc_slice_str(str1, num, itc_len(str1));
+    newstr += itc_slice_str(str1, num + 1, itc_len(str1));
     return newstr;
 }
