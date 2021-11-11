fc4d2107709230dd066634e9336e0ee791301524
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2021-11-11 11:34:33 +0300

    Update ft_even_place.cpp

--- ft_even_place.cpp
+++ ft_even_place.cpp
@@ -2,7 +2,7 @@
 
 string itc_even_place(string str){
     string newstr;
-    for(int i = 1; i < itc_len(str); i+=2){
+    for(long long i = 1; i < itc_len(str); i+=2){
         newstr += str[i];
     }
     return newstr;
